4621d800adffb5adf30df9f9e1f182c606cbecfd
Ruthvika M <ruthvika.motrapu@gmail.com>
2026-09-13 21:53:47 +0530

    Remove second.js

--- second.js
+++
@@ -1,7 +0,0 @@
-var http= require("node:http");
-const port = 3000;
-const server = http.createServer((req,res)=>{
-    res.setHeader('Content-Type','text/plain');
-    res.end('Hello World from Node.js Web Server!\n');
-});
-server.listen(port);
